8462b9cea284b97b082e9048e01f716af4a46118
André Mateus <95716954+Andre-Mateus-Naveca@users.noreply.github.com>
2022-10-14 18:14:59 -0400

    Add files via upload

---
+++ bottle.js
@@ -0,0 +1,27 @@
+function preload()
+{
+    img = loadImage("garrafa.jpg");
+}
+function setup()
+{
+    canvas = createCanvas(640, 420)
+    canvas.center()
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detectando Objetos";
+}
+function modelLoaded() {
+    console.log("Modelo Carregado!")
+    status1 = true;
+    objectDetector.detect(img, gotResults)
+}
+function gotResults(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+function voltar()
+{
+    window.location = "index.html"
+}
